1f4d271421dbbbc8332b1e0fe4321642edd78441
Dharsini21 <71117529+Dharsini21@users.noreply.github.com>
2021-06-26 22:59:54 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Web development</h1>
+      <p>a basic knowlegde of Html,css,javascript,react.js and node.js </p>
+    </div>
+  );
+}
+export default Info;
